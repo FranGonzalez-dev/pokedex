e3a4ec895376cd5b1e765488f0a20992ff919435
Fran <gonzalezf503@gmail.com>
2022-06-30 17:24:20 -0300

    PokeDetails created

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './scss/index.scss'
 
 
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
--- src/pages/PokeDetails.jsx
+++ src/pages/PokeDetails.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
-import { getPokemonData } from '../helpers';
+import { getPokemonData, getPokemonSpecies } from '../helpers';
 import { TypeIcons } from '../components';
+import { ProgressBar } from 'react-bootstrap';
 
 import styles from '../scss/modules/PokeDetails.module.scss'
 
@@ -10,13 +11,16 @@ import styles from '../scss/modules/PokeDetails.module.scss'
 export const PokeDetails = () => {
 
     const [ pokemon, setPokemon ] = useState();
+    const [species, setSpecies] = useState();
     const [ loading, setLoading ] = useState(true);
     
     const { id } = useParams();
 
     const getPokemon = async (id) => {
         const details = await getPokemonData(id);
+        const species = await getPokemonSpecies(id);
         setPokemon(details.data);
+        setSpecies(species.data);
         setLoading(false);
     }
     
@@ -56,19 +60,39 @@ return (
                 </div>
             </div>
             <div className={styles.details}>
+
                 <div className={styles.containerImg}>
                     <img src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name}/>
                 </div>
-                <p>{}</p>
-                <div className='stats-container'>                    
-                    {pokemon.stats.map( s => {
-                    return (
-                        <div className='stats' key={s.stat.name}>
-                            <p><span>{s.stat.name}:</span> {s.base_stat}</p>
+
+
+                <h2>About</h2>
+
+                <p>{species.flavor_text_entries[0].flavor_text}</p>
+                <p>{species.generation.name}</p>
+                <p> Height: { pokemon.height / 10}mt </p>
+                <p> Weight { pokemon.weight / 10 }kg </p>
+                
+                
+                {   pokemon.abilities.map( a => {
+                        return (
+                            <p> {a.ability.name} </p>
+                        )
+                    })
+                }
+                
+                <div>
+                    <h2>Stats</h2>
+                    {pokemon.stats.map( s =>  (
+                        <div className={styles.stats} key={s.stat.name}>
+                            <p>{s.stat.name}</p>
+                            <ProgressBar now={s.base_stat} max={255} label={s.base_stat}/>
                         </div>
                     )
-                    })}
-                </div>                
+                    )}
+                </div>
+                
+                
             </div>
 
         </div>
